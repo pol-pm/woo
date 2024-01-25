c49e3a5255d0d8515a5f4a51ad2e0f0af95b3e8a
Jacek Poleszak <j@pol.pm>
2024-01-25 12:25:27 +0000

    Create jkp-nowa-zakladka.php

---
+++ jkp-nowa-zakladka.php
@@ -0,0 +1,22 @@
+/**
+ * @kod             Dodaje zakładkę Dokumentacja do pojedynczego produktu @ WooCommerce
+ * @autor           Jacek Poleszak
+ * @kompatybilnosc  WooCommerce 8
+ * @spolecznosc     https://grupa.jacekpoleszak.pl/
+ */
+
+add_filter( 'woocommerce_product_tabs', 'jkpagency_dodaj_zakl_dok_do_produktow' );
+
+function jkpagency_dodaj_zakl_dok_do_produktow( $tabs ) {
+  // Dodaj zakładkę Dokumentacja do produktu
+  $tabs['dokumentacja'] = array(
+    'title' => __( 'Dokumentacja', 'woocommerce' ),
+    'priority' => 19,
+    'callback' => 'zakl_dok_zawartosc'
+  );
+  return $tabs;
+}
+
+function zakl_dok_zawartosc() {
+  echo 'Tu umieść zawartość zakładki z Dokumentacją.';
+}
